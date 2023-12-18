c19b8253992ec359a594de27dfb3521930822d11
Paweł Brzozowski <pawel@brzoza.net>
2023-12-18 14:16:06 +0100

    Create Loader component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,13 @@ import { Component } from "react";
 import { ImageGallery } from "./ImageGallery";
 import { Searchbar } from "./Searchbar/Searchbar";
 import { getPhotos } from "../services/pixabayAPI";
+import { Loader } from "./Loader/Loader";
 // import css from "./App.module.css";
 
 export class App extends Component {
   state = {
     photos: [],
+    isLoading: false,
   };
   // constructor(props) {
   //   super(props);
@@ -16,8 +18,11 @@ export class App extends Component {
 
   handleSearch = async query => {
     try {
+      this.setState({
+        isLoading: true,
+        photos: [],
+      });
       const photos = await getPhotos(query);
-      // debugger;
       this.setState({
         photos,
       });
@@ -25,6 +30,10 @@ export class App extends Component {
       console.log(photos);
     } catch (error) {
       console.error(`Wystąpił błąd: ${error}`);
+    } finally {
+      this.setState({
+        isLoading: false,
+      });
     }
   };
 
@@ -32,6 +41,7 @@ export class App extends Component {
     return (
       <>
         <Searchbar handleSearch={this.handleSearch} />
+        {this.state.isLoading && <Loader />}
         <ImageGallery data={this.state.photos} />
       </>
     );
--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,1 +1,19 @@
-import css from "./Loader.module.css";
+import { ThreeCircles } from "react-loader-spinner";
+// import css from "./Loader.module.css";
+
+export const Loader = () => {
+  return (
+    <ThreeCircles
+      height="100"
+      width="100"
+      color="#4fa94d"
+      wrapperStyle={{}}
+      wrapperClass=""
+      visible={true}
+      ariaLabel="three-circles-rotating"
+      outerCircleColor=""
+      innerCircleColor=""
+      middleCircleColor=""
+    />
+  );
+};
