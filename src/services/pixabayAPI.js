603afbcfaf34c97a95ec1e80cb804b1687d97f5b
Paweł Brzozowski <pawel@brzoza.net>
2023-12-18 14:35:29 +0100

    Add Promise / setTimeout

--- src/services/pixabayAPI.js
+++ src/services/pixabayAPI.js
@@ -5,8 +5,20 @@ const API_KEY = "8543283-ac41910cbcd5ccb3a6a09e0db";
 const PAGE = 1;
 const IMG_PER_PAGE = 12;
 
+const FETCH_DELAY = 1000;
+
 export const getPhotos = async query => {
-  const url = `${BASE_URL}?q=${query}&key=${API_KEY}&page=${PAGE}&per_page=${IMG_PER_PAGE}`;
-  const response = await axios.get(url);
-  return response.data.hits;
+  return new Promise(res => {
+    setTimeout(async () => {
+      const url = `${BASE_URL}?q=${query}&key=${API_KEY}&page=${PAGE}&per_page=${IMG_PER_PAGE}`;
+      const response = await axios.get(url);
+      res(response.data.hits);
+    }, FETCH_DELAY);
+  });
 };
+
+// export const getPhotos = async query => {
+//   const url = `${BASE_URL}?q=${query}&key=${API_KEY}&page=${PAGE}&per_page=${IMG_PER_PAGE}`;
+//   const response = await axios.get(url);
+//   return response.data.hits;
+// };
