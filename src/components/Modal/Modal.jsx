2699594ded78cbf79eb4de73a5ce57b0086a68f0
Paweł Brzozowski <pawel@brzoza.net>
2024-01-20 16:40:11 +0100

    Add prop-types

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -44,6 +44,6 @@ export class Modal extends Component {
 
 Modal.propTypes = {
   toggleIsModalVisible: PropTypes.func.isRequired,
-  // largeImageURL: PropTypes.string.isRequired,
-  // tags: PropTypes.string.isRequired,
+  largeImageURL: PropTypes.string.isRequired,
+  tags: PropTypes.string.isRequired,
 };
